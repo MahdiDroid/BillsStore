d6607db8193e8f2e652db77476904ab30994466d
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-22 10:57:18 +0300

    healthy burger implemented

---
+++ src/com/company/HealthyBurger.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class HealthyBurger extends  Hamburger{
+
+    private int maxNumberOfAddition;
+
+    public HealthyBurger( double basePrice, int maxNumberOfAddition) {
+        super("healthyHamburger", "Jambon", basePrice, "brown",6);
+
+    }
+}
+
+
